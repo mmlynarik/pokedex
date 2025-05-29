0b99e5f19c6f86f848987c0f0626a17057bff4c1
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-05-29 17:53:11 +0200

    First django models

--- src/djangoproject/api/admin.py
+++ src/djangoproject/api/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Pokemon, PokemonSpecies, PokemonType
+
+
+class PokemonTypeAdmin(admin.ModelAdmin):
+    ordering = ("id",)
+    list_display = [
+        "id",
+        "name",
+    ]
+
+
+class PokemonSpeciesAdmin(admin.ModelAdmin):
+    ordering = ("id",)
+    list_display = [
+        "id",
+        "name",
+    ]
+
+
+class PokemonAdmin(admin.ModelAdmin):
+    ordering = ("id",)
+    list_display = [
+        "id",
+        "name",
+        "pokedex_no",
+        "species",
+        "weight",
+    ]
+    list_filter = ["species"]
+    filter_horizontal = ("types",)
+
+
+admin.site.register(PokemonType, PokemonTypeAdmin)
+admin.site.register(PokemonSpecies, PokemonSpeciesAdmin)
+admin.site.register(Pokemon, PokemonAdmin)
--- src/djangoproject/api/models.py
+++ src/djangoproject/api/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
 
-class Department(models.Model):
-    title = models.CharField(max_length=100)
+class PokemonType(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
 
 
-class Employee(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    birthdate = models.DateField(null=True, blank=True)
-    cv = models.FileField(null=True, blank=True)
+class PokemonSpecies(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Pokemon(models.Model):
+    pokedex_no = models.IntegerField()
+    name = models.CharField(max_length=64)
+    types = models.ManyToManyField(PokemonType)
+    species = models.ForeignKey(PokemonSpecies, on_delete=models.CASCADE)
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}"
