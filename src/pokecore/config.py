23cc3cc16a68c3168362d72ba37f97199e9038c3
Miroslav Mlynarik <mmlynarik@sk.uss.com>
2025-05-30 20:44:11 +0200

    api

--- src/pokecore/config.py
+++ src/pokecore/config.py
@@ -1,1 +1,1 @@
-BASE_POKEMON_API_URL="https://pokeapi.co/api/v2"
+BASE_POKEMON_API_URL="https://pokeapi.co/api/v2/"
--- src/pokecore/pokeapi.py
+++ src/pokecore/pokeapi.py
@@ -1,13 +1,24 @@
-from pathlib import Path
+from pprint import pprint
+from typing import Any
 import requests
-from pokedexcore.config import BASE_POKEMON_API_URL
+from pokecore.config import BASE_POKEMON_API_URL
+from urllib.parse import urljoin
 
 
-def get_pokemon_types():
-    types_url = Path(BASE_POKEMON_API_URL, "type")
-    print(types_url)
-    res = requests.get("https://pokeapi.co/api/v2/pokemon/pikachu")
-    return res.json()
+def get_limit_query_param() -> str:
+    return "?limit=100000"
+
+def get_types_url() -> str:
+    types_url = urljoin(BASE_POKEMON_API_URL, "type")
+    limit_query = get_limit_query_param()
+    unlimited_url = urljoin(types_url, limit_query)
+    return unlimited_url
 
 
-get_pokemon_types()
+def get_pokemon_types() -> dict[str, Any]:
+    types_url = get_types_url()
+    data = requests.get(types_url).json()
+    return data
+
+
+pprint(get_pokemon_types())
