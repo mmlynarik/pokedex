f3045f3d455f40a00d26de1042179c811db846fe
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-31 01:28:33 +0200

    First db import

--- src/djangoproject/pokeapp/management/commands/importpokeapi.py
+++ src/djangoproject/pokeapp/management/commands/importpokeapi.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Import Pokemon API data"
+    help = "Import basic Pokemon data from Pokemon API (pokeapi.co)"
 
     def handle(self, *args, **options):
         pokemon_types = [PokemonType(name=t.name) for t in get_pokemon_types()]
