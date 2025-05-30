d4feea6f6b36f183a35f29a7f76b64f46963e47b
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-05-30 21:40:29 +0200

    Dockerignore

--- src/pokecore/pokeapi.py
+++ src/pokecore/pokeapi.py
@@ -1,12 +1,15 @@
 from pprint import pprint
 from typing import Any
-import requests
-from pokecore.config import BASE_POKEMON_API_URL
 from urllib.parse import urljoin
+
+import requests
+
+from pokecore.config import BASE_POKEMON_API_URL
 
 
 def get_limit_query_param() -> str:
     return "?limit=100000"
+
 
 def get_pokemon_types_url() -> str:
     types_url = urljoin(BASE_POKEMON_API_URL, "type")
