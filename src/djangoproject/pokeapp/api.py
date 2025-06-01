b39c749682df0ad68672840e5ac81427784d3b38
Miroslav Mlynarik <miroslav.mlynarik@gmail.com>
2025-06-01 11:58:56 +0200

    Pokedex GET endpoint

--- src/djangoproject/pokeapp/api.py
+++ src/djangoproject/pokeapp/api.py
@@ -1,11 +1,38 @@
-from ninja import Router
-from pokeapp.models import PokemonForm
+from ninja import Router, Schema
+from pokeapp.models import Pokemon, PokemonForm
 
 router = Router()
 
 
-@router.get("/forms/{form}")
-def get_pokemon_form(request, form: str):
-    PokemonForm.objects.filter(name=form)
+class PokemonDetail(Schema):
+    pokedex_no: int
+    name: str
+    types: list[str]
+    weight: int
+    height: int
+    is_default: bool
 
-    # return [{"id": p.pk, "pokedex_no": p.pokedex_no} for p in Pokemon.objects.all()]
+
+class PokemonNotFound(Schema):
+    msg: str
+
+
+@router.get(
+    "/pokemons/{name}",
+    response={200: PokemonDetail, 404: PokemonNotFound},
+    summary="Retrieve one Pokemon or PokemonForm from Pokedex",
+)
+def get_pokemon_form(request, name: str):
+    pokemon_form = PokemonForm.objects.filter(form=name).first()
+    if not pokemon_form:
+        return 404, PokemonNotFound(msg=f"Pokemon {name} was not found. Check the spelling and try again.")
+
+    pokemon = Pokemon.objects.get(name=pokemon_form.form)
+    return {
+        "pokedex_no": pokemon.pokedex_no,
+        "name": pokemon_form.form,
+        "types": pokemon.types.all().values_list("name", flat=True),
+        "weight": pokemon.weight,
+        "height": pokemon.height,
+        "is_default": pokemon_form.is_default,
+    }
